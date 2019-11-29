021ab3f0a97e1f71b0bc4b05dfa8196fee866e13
programador-gnt <programador@gnt.pe>
2019-11-29 13:13:36 -0500

    BD

--- blog.sql
+++ blog.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-11-2019 a las 02:08:50
--- Versión del servidor: 10.3.16-MariaDB
--- Versión de PHP: 7.1.30
+-- Tiempo de generación: 29-11-2019 a las 19:13:14
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -92,7 +92,8 @@ INSERT INTO `post` (`id_post`, `titulo`, `cuerpo`, `categoria`, `imagen`, `autor
 (2, 'Cometa', 'Los cometas son los cuerpos celestes constituidos por hielo, polvo y rocas que orbitan alrededor del Sol siguiendo diferentes trayectorias elípticas, parabólicas o hiperbólicas. Los cometas, junto con los asteroides, planetas y satélites, forman parte del sistema solar. La mayoría de estos cuerpos celestes describen órbitas elípticas de gran excentricidad, lo que produce su acercamiento al Sol con un período considerable. A diferencia de los asteroides, los cometas son cuerpos sólidos compuestos de materiales que se subliman en las cercanías del Sol. A gran distancia (a partir de 5-10 UA) desarrollan una atmósfera que envuelve al núcleo, llamada coma o cabellera, que está formada por gas y polvo. A medida que el cometa se acerca al Sol, el viento solar azota la coma y se genera la cola característica, la cual está formada por polvo y el gas de la coma ionizado.', 'Curiosidades', 'https://i.imgur.com/eI0yeJg.jpg', 'betavirosis', '25 agos'),
 (3, 'Lasaña', 'La lasaña (italiano: lasagne) es un tipo de pasta que se sirve en láminas, además de denominarse así también a un plato que tiene pasta en láminas intercaladas con carne (ragú o salsa boloñesa) y bechamel llamado lasaña al horno (Lasagna al forno). Se trata de un plato de origen italiano.1​ La lasaña al horno también se puede hacer con verduras (espinacas, berenjenas, etc.) o pescados.2​ Se termina con bechamel y abundante queso rallado para gratinarla en el horno. Ambos platos tienen como lugar de origen Italia. La palabra \"lasaña\" proviene del griego \"lasanon\", a través del latín \"lasanum\", que se refiere al pote en el que se cocinaba. La palabra singular en italiano es lasagna y en plural lasagne se aplica indistintamente al plato o a la pasta en forma de láminas. Es una entrada o primer plato caliente que se suele comer en invierno o en los periodos fríos de la primavera.', 'Estilo de vida', 'https://i.imgur.com/ThDvbgv.jpg', 'Betavirosis', '25 agos'),
 (4, 'La Galaxia', 'Históricamente, las galaxias se han clasificado de acuerdo a su forma aparente (morfología visual). Una forma común es la galaxia elíptica que, como lo indica su nombre, tiene el perfil luminoso de una elipse. Las galaxias espirales tienen forma circular pero con estructura de brazos curvos envueltos en polvo. Las galaxias inusuales se llaman galaxias irregulares y son, normalmente, el resultado de perturbaciones provocadas por la atracción gravitacional de galaxias vecinas. Estas interacciones entre galaxias vecinas, que pueden provocar la fusión de galaxias, pueden inducir el intenso nacimiento de estrellas. Finalmente, tenemos las galaxias pequeñas, que carecen de una estructura coherente y también se las llama galaxias irregulares.', 'Curiosidades', 'https://i.imgur.com/DmEbCcE.jpg', 'Betavirosis', '11 oct'),
-(5, 'Nuestro Sol', 'Se formó hace aproximadamente 4600 millones de años a partir del colapso gravitacional de la materia dentro de una región de una gran nube molecular. La mayor parte de esta materia se acumuló en el centro, mientras que el resto se aplanó en un disco en órbita que se convirtió en el sistema solar. La masa central se volvió cada vez más densa y caliente, dando lugar con el tiempo al inicio de la fusión nuclear en su núcleo. Se cree que casi todas las estrellas se forman por este proceso. El Sol es más o menos de edad intermedia y no ha cambiado drásticamente desde hace más de cuatro mil millones de años, y seguirá siendo bastante estable durante otros cinco mil millones de años más. Sin embargo, después de que la fusión del hidrógeno en su núcleo se haya detenido, el Sol sufrirá cambios importantes y se convertirá en una gigante roja. Se estima que el Sol se volverá lo suficientemente grande como para engullir las órbitas actuales de Mercurio, Venus y posiblemente la Tierra.', 'Curiosidades', 'https://i.imgur.com/PMAoNUb.jpg', 'Betavirosis', '14 Oct');
+(5, 'Nuestro Sol', 'Se formó hace aproximadamente 4600 millones de años a partir del colapso gravitacional de la materia dentro de una región de una gran nube molecular. La mayor parte de esta materia se acumuló en el centro, mientras que el resto se aplanó en un disco en órbita que se convirtió en el sistema solar. La masa central se volvió cada vez más densa y caliente, dando lugar con el tiempo al inicio de la fusión nuclear en su núcleo. Se cree que casi todas las estrellas se forman por este proceso. El Sol es más o menos de edad intermedia y no ha cambiado drásticamente desde hace más de cuatro mil millones de años, y seguirá siendo bastante estable durante otros cinco mil millones de años más. Sin embargo, después de que la fusión del hidrógeno en su núcleo se haya detenido, el Sol sufrirá cambios importantes y se convertirá en una gigante roja. Se estima que el Sol se volverá lo suficientemente grande como para engullir las órbitas actuales de Mercurio, Venus y posiblemente la Tierra.', 'Curiosidades', 'https://i.imgur.com/PMAoNUb.jpg', 'Betavirosis', '14 Oct'),
+(6, 'Super Mario Bros.', 'Super Mario Bros. o Super Mario Brothers (スーパーマリオブラザーズ Sūpā Mario Burazāzu?, lit. Súper Hermanos Mario) es un videojuego de plataformas, diseñado por Shigeru Miyamoto, lanzado el 13 de septiembre de 1985 y producido por la compañía Nintendo, para la consola Nintendo Entertainment System (NES). El juego describe las aventuras de los hermanos Mario y Luigi, personajes que ya protagonizaron el arcade Mario Bros. de 1983. En esta ocasión ambos deben rescatar a la Princesa Peach del Reino Champiñón que fue secuestrada por el rey de los Koopas, Bowser. A través de ocho diferentes mundos, los jugadores pueden controlar a alguno de los dos hermanos y deben enfrentarse finalmente tras los niveles correspondientes de cada mundo a los monstruos de cada castillo para rescatar a la Princesa Peach.\r\n\r\nSuper Mario Bros. fue el juego que popularizó al personaje de Mario​, convirtiéndolo en el ícono principal de Nintendo, y uno de los personajes más reconocidos de los videojuegos, así como su hermano menor Luigi. Además, presentó por primera vez a la Princesa Peach Toadstool, Toad, Bowser, entre otros personajes. Este juego es considerado el primer videojuego de plataformas de desplazamiento lateral de Nintendo y se ha convertido en un hito debido a la trascendencia de su diseño y papel en la industria de los videojuegos. Su lanzamiento fue el primer gran récord de ventas posterior a la crisis de la industria de los videojuegos de 1983 (alcanzando más de 10 millones de cartuchos vendidos), por lo que popularizó, en cierta manera, a la consola NES. En 1999 fue reconocido como uno de los videojuegos más vendidos de todos los tiempos.​ Shigeru Miyamoto su creador, relata la existencia de Super Mario Bros. debido a que cuando viajaba en un tren le gustaba mirar por la ventana, y se imaginaba que estaba allí saltando afuera.​ Debido a la popularidad de Mario, se encuentra también en otros videojuegos como el original Donkey Kong y Super Smash Bros.\r\n\r\nTras su éxito comercial, tuvo una secuela directa, conocida en fuera de Japón como Super Mario Bros.: The Lost Levels, ya que en occidente se comercializó como la secuela directa una variación de otro juego en el que había participado Miyamoto, Yume Koujou Doki Doki Panic!, debido a que se consideró la versión lanzada en Japón como demasiado difícil. Tuvo también una variedad de versiones «alternas», como All Night Nippon Super Mario Bros., así como adaptaciones a arcade, películas y series de televisión.\r\n\r\nEl 13 de septiembre de 2010 se conmemoró el 25º aniversario del juego desde su salida para la Famicom en Japón, así como el 25° aniversario de la franquicia. En noviembre de 2010 la ciudad española de Zaragoza dedicó una avenida al famoso fontanero.', 'Curiosidades', 'https://i.imgur.com/Bh0wlB6.jpg', 'Betavirosis', '29 Nov');
 
 -- --------------------------------------------------------
 
@@ -220,7 +221,45 @@ INSERT INTO `visitas` (`id_visita`, `id_post`) VALUES
 (88, 2),
 (89, 4),
 (90, 4),
-(91, 4);
+(91, 4),
+(92, 5),
+(93, 5),
+(94, 5),
+(95, 5),
+(96, 3),
+(97, 3),
+(98, 2),
+(99, 2),
+(100, 5),
+(101, 5),
+(102, 1),
+(103, 1),
+(104, 1),
+(105, 1),
+(106, 1),
+(107, 1),
+(108, 1),
+(109, 1),
+(110, 1),
+(111, 1),
+(112, 1),
+(113, 1),
+(114, 1),
+(115, 1),
+(116, 1),
+(117, 1),
+(118, 1),
+(119, 1),
+(120, 1),
+(121, 1),
+(122, 1),
+(123, 1),
+(124, 1),
+(125, 1),
+(126, 6),
+(127, 6),
+(128, 6),
+(129, 6);
 
 --
 -- Índices para tablas volcadas
@@ -288,7 +327,7 @@ ALTER TABLE `suscripcion`
 -- AUTO_INCREMENT de la tabla `visitas`
 --
 ALTER TABLE `visitas`
-  MODIFY `id_visita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id_visita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
